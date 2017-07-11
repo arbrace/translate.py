c8ee57a6450b8b7499ad0a27dfba747c122fbbc1
Aaron R Brace <aaron.r.brace@gmail.com>
2017-07-11 14:31:14 +0100

    Create translate_csv.py

---
+++ translate_csv.py
@@ -0,0 +1,25 @@
+import csv
+from yandex_translate import YandexTranslate #make sure this is imported
+# this is your api key
+translate = YandexTranslate('<YandexAPIKey>')
+print('Languages:', translate.langs)
+
+# read your csv, single col to be translated
+with open('C:\\Users\\aaron.brace\Desktop\\qs3.csv', 'r') as f:
+  reader = csv.reader(f)
+  your_list = list(reader)
+
+print('Detect language:', translate.detect(your_list))
+# detects the current language
+trans= translate.detect(your_list)
+print(trans)
+print(len(your_list))
+print(your_list[1])
+
+
+# loop over your list, translate from detected language to english.
+counter = 0
+while counter < len(your_list):
+    lang = translate.detect(your_list[counter]),
+    print('Translate:', translate.translate(your_list[counter], '%s-en' %lang))
+    counter = counter+1
